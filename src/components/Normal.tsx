64e4358226561cb020ed01f13d4932091e85248f
murtuzaalisurti <murtuza.creativity@gmail.com>
2023-03-14 15:27:44 +0530

    removed unnecessary imports

--- src/components/Normal.tsx
+++ src/components/Normal.tsx
@@ -1,17 +1,17 @@
-import {useRef, useState} from 'react'
+import { useState } from 'react'
 
 const Normal = () => {
     const [count, setCount] = useState(0)
-  return (
-    <div className='comp-contain'>
-        <h3>Normal counter implementation in React</h3>
-        <p>{count}</p>
-        <div className="controls">
-            <button onClick={() => setCount(count => count + 1)}>+</button>
-            <button onClick={() => setCount(count => count - 1)}>-</button>
+    return (
+        <div className='comp-contain'>
+            <h3>Normal counter implementation in React</h3>
+            <p>{count}</p>
+            <div className="controls">
+                <button onClick={() => setCount(count => count + 1)}>+</button>
+                <button onClick={() => setCount(count => count - 1)}>-</button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Normal
